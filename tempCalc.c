0deacee589c1c9ac8a79280888b123ec78b6a6fe
Jim Stanton <jim@jhstanton.com>
2016-02-21 16:07:56 -0600

    Small doxygen updates

--- tempCalc.c
+++ tempCalc.c
@@ -1,9 +1,16 @@
+/**
+ * @file tempCalc.c
+ * 
+ * This modules handles values sampled by from the thermistor
+ */
+
 #define DESD_TEMP // the constant temp to hold the black box
 #define TEMP_THRESH // the allowable threshold (high or low) for temperature 
 #define SMP_DELAY // the time between individual samples in the sample set
-#define WINDOW_Q // the queue structure holding the window of samples
 #define CURR_INDEX // the current index of the beginning of the sample window in the queue
 #define NUM_SAMPLES // number of samples to take before taking the average temp
+
+typdef int* WINDOW_Q ; // the queue structure holding the window of samples
 
 /**
  * This module calculates the sampled average temperature for each
